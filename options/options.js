8c6f82a64e54d8ef02a4e65ba95d2877a25aaaff
Crowdbotics <team@crowdbotics.com>
2021-08-05 16:26:41 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "super_paper_29515Identifier",
+  url: "https://super_paper_29515Identifier.botics.co",
+  api: "https://super_paper_29515Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
